85792ef7c669f2b91168a0c9d047285bd29b1b24
KritikaSharmaKS <sh.kritika01@gmail.com>
2021-04-20 22:14:32 -0400

    select from where example completed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Order = require("./models/order");
 
 Customer.hasMany(Order);
 
+let customerId = null;
 sequelize
   .sync({ force: true })
   //   .sync()
@@ -12,10 +13,15 @@ sequelize
     return Customer.create({ name: "Chandler Bing", email: "cb@gmail.com" });
   })
   .then((customer) => {
+    customerId = customer.id;
     console.log("Customer created, id is : ", customer.id);
     return customer.createOrder({ total: 34.99 });
   })
   .then((order) => {
     console.log("Order created, id is : ", order.id);
+    return Order.findAll( { where: { customerId } });
+  })
+  .then((orders) => {
+    console.log(orders);
   })
   .catch((err) => console.error(err));
--- models/customer.js
+++ models/customer.js
@@ -1,5 +1,4 @@
 const Sequelize = require("sequelize");
-
 const sequelize = require("../util/database");
 
 const Customer = sequelize.define("customer", {
