a5f4ecd900f823e52a056e7001f9f82f9dd7d9b6
KritikaSharmaKS <sh.kritika01@gmail.com>
2021-05-02 20:25:32 -0400

    change the db name

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("sequelize-youtube", "root", "9871709924", {
+const sequelize = new Sequelize("ks-store", "root", "9871709924", {
   dialect: "mysql",
   host: "localhost",
 });
